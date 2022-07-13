7479a40085d07f0a6db132cf0cfcab3e5e2f8d5e
Mürsel <murselcetin51@gmail.com>
2022-07-13 22:58:16 +0300

    Bottom nav düzenlemeleri yapıldı.

--- app/src/main/java/com/example/yiyo/MainActivity.kt
+++ app/src/main/java/com/example/yiyo/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.yiyo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.navigation.Navigation
+
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.yiyo.databinding.ActivityMainBinding
 import com.example.yiyo.util.gecisYap
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,7 +23,5 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         NavigationUI.setupWithNavController(binding.bottomNavBar, navHostFragment.navController)
-        binding.bottomNavBar.background = null
-        binding.bottomNavBar.menu.getItem(1).isEnabled = false
     }
 }
